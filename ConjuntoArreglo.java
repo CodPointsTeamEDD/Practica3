d39f4f829cc864997af56df8b369f13bc296bcc8
Erick-XMB <erick.martinezkmn@ciencias.unam.mx>
2026-02-24 11:05:03 -0600

    ola

---
+++ ConjuntoArreglo.java
@@ -0,0 +1,76 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class ConjuntoArreglo<T> extends Conjunto<T>{
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int indice = 0;
+
+            @Override
+            public boolean hasNext() {
+                return indice < ConjuntoArreglo.this.elementos.length;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return elementos[indice++];
+            }
+        };
+    }
+
+    private T[] elementos;
+
+    public ConjuntoArreglo() {
+        this.elementos = (T[]) new Object[0];
+    }
+
+    public ConjuntoArreglo(T[] elementos) {
+        if (elementos == null) {
+            this.elementos = (T[]) new Object[0];
+        } else if (UtilArreglos.tieneDuplicados(elementos)) {
+            throw new IllegalArgumentException("El arreglo contiene elementos duplicados.");
+        } else if (UtilArreglos.contieneNull(elementos)) {
+            throw new IllegalArgumentException("El arreglo contiene elementos nulos.");
+        } else {
+            this.elementos = UtilArreglos.copiaArregloGenerico(elementos);
+        }
+    }
+
+    public boolean pertenece(T elemento) {
+        /*Aquí va tu código*/
+    }
+
+    public void agregarElemento(T elemento) {
+        /*Aquí va tu código*/
+    }
+    
+    public void eliminarElemento(T elemento){
+    	/*Aquí va tu código*/
+    }
+
+    public boolean contieneConjunto(Conjunto<T> c) {
+        /*Aquí va tu código*/
+    }
+
+    public Conjunto<T> union(Conjunto<T> c) {
+        /*Aquí va tu código*/
+    }
+
+    public Conjunto<T> interseccion(Conjunto<T> c) {
+        /*Aquí va tu código*/
+    }
+
+    public boolean iguales(Conjunto<T> c) {
+        /*Aquí va tu código*/
+    }
+
+    public int obtenerCardinalidad(){
+        /*Aquí va tu código*/
+    }
+
+}
